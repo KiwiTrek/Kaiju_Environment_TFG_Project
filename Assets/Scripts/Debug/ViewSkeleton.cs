91c4540efa84c03ed571f8ac115624043e592a54
IsaaColomer <willytrek20@gmail.com>
2023-03-14 19:22:17 +0100

    Simpler collisions & changing arena structure

---
+++ Assets/Scripts/Debug/ViewSkeleton.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ViewSkeleton : MonoBehaviour
+{
+    public Transform rootNode;
+    public Transform[] childNodes;
+
+    void OnDrawGizmosSelected()
+    {
+        if (rootNode != null)
+        {
+            if (childNodes == null)
+            {
+                //get all joints to draw
+                PopulateChildren();
+            }
+
+
+            foreach (Transform child in childNodes)
+            {
+
+                if (child == rootNode)
+                {
+                    //list includes the root, if root then larger, green cube
+                    Gizmos.color = Color.green;
+                    Gizmos.DrawCube(child.position, new Vector3(.1f, .1f, .1f));
+                }
+                else
+                {
+                    Gizmos.color = Color.blue;
+                    Gizmos.DrawLine(child.position, child.parent.position);
+                    Gizmos.DrawCube(child.position, new Vector3(.01f, .01f, .01f));
+                }
+            }
+
+        }
+    }
+
+    public void PopulateChildren()
+    {
+        childNodes = rootNode.GetComponentsInChildren<Transform>();
+    }
+}
